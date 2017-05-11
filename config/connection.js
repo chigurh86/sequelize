4f8a8f2483a1501adc8448c9d572fc3c7038d447
Antonio Caos Jr <antoniocaosjr.@Antonios-MacBook-Pro.local>
2017-05-10 23:02:00 -0400

    uncommented connection

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
 var mysql = require("mysql");
 var connection;
 //
-// if (process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-//
-// } else{
-//   var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "",
-//     database: "burgers_db"
-//   });
-// };
-//
+
+} else{
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+};
+
 connection.connect();
 //
 module.exports = connection;
